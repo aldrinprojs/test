dbdd4512a6975e5199625ac572eac1973794e129
aldrinprojs <aldrinitono0114@gmail.com>
2025-10-03 22:39:09 +0800

    first commit

--- src/AppVariables.ts
+++
@@ -1,6 +0,0 @@
-class AppVariables{
-    //static readonly ApiUrl:string = "https://localhost:7133";
-    static readonly ApiUrl:string = "https://www.rayanestaszewski.fr";
-}
-
-export default AppVariables;
